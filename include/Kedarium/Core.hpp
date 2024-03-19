d523dba2a37163a11a5d581fff941b5c5fe440c1
marcothelost <marcothelost@proton.me>
2024-03-19 07:02:30 +0100

    Creating the Manager class

--- include/Kedarium/Core.hpp
+++ include/Kedarium/Core.hpp
@@ -4,6 +4,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <map>
+#include <string>
 
 #include "Constants.hpp"
 
@@ -41,6 +43,59 @@ namespace kdr
      * @brief Terminates GLFW.
      */
     void terminate();
+
+    /**
+     * @brief A generic manager class for storing and retrieving items by key.
+     * 
+     * @tparam T The type of items stored in the manager.
+     */
+    template<class T>
+    class Manager
+    {
+      public:
+        /**
+         * @brief Adds an item to the manager with the specified key.
+         * 
+         * @param key The key associated with the item.
+         * @param value The value of the item to add.
+         */
+        void add(const std::string& key, const T& value)
+        {
+          this->items[key] = value;
+        }
+
+        /**
+         * @brief Retrieves an item from the manager by its key.
+         * 
+         * If the item with the specified key is found, it is returned.
+         * If the item is not found, a default value of type T is returned and an error message is printed.
+         * 
+         * @param key The key of the item to retrieve.
+         * @return A reference to the retrieved item.
+         */
+        T& get(const std::string& key)
+        {
+          auto it = items.find(key);
+          if (it != items.end())
+          {
+            return it->second;
+          }
+          std::cerr << "Item with key \"" << key << "\" not found!\n";
+          static T default_value;
+          return default_value;
+        }
+
+        /**
+         * @brief Clears all items stored in the manager.
+         */
+        void clear()
+        {
+          this->items.clear();
+        }
+
+      private:
+        std::map<std::string, T> items;
+    };
   }
 }
 
